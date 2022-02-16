f34671f322b849b01d2eda4e04c390763731383c
Henrique <lhcr@inf.ufsm.br>
2022-02-16 16:05:35 -0300

    commit

--- python2.py
+++ python2.py
@@ -1,3 +1,3 @@
 n1=int(input('n1: '))
 n2=int(input('n2: '))
-print('n1 potência de n2= {} , n1 div inteira n2 = {}, n1 modulo de n2 = {}'.format(n1**n2,n1//n2,n1%n2))
+#print('n1 potência de n2= {} , n1 div inteira n2 = {}, n1 modulo de n2 = {}'.format(n1**n2,n1//n2,n1%n2))
