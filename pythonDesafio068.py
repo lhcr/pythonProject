0758027ce87c0cdfa11f55f10fbe90f20f9968c4
Henrique <lhcr@inf.ufsm.br>
2022-03-08 11:13:36 -0300

    commit

--- pythonDesafio068.py
+++ pythonDesafio068.py
@@ -1,16 +1,18 @@
 from tokenize import String
 
 from random import randint
-n = s =0
+n = s = res = 0
 while True:
     n = int(input('Digite um número: '))
     escolha = str(input('Par(P) ou Impar(I): '))
-    n2 = int(randint)
+    n2 = int(randint(0,100))
     res = (n+n2) % 2
-    if  res == 0 and escolha != 'P':
-        break
-    elif res !=0 and escolha != 'I':
-        break
+    if  res == 0 and escolha != 'p':
+        print(f'Você perdeu meu número é {n2} e você escolheu {escolha} o res foi {res}')
+        break;
+    elif res !=0 and escolha != 'i':
+        print(f'Você perdeu meu número é {n2} e você escolheu {escolha} o res foi {res}')
+        break;
     print('Você ganhou')
     s+=1
 print(f'Você venceu {s} vezes antes de perder. ')
